1df98eae765462f41b1a38b7e85cd5db7fff0e3a
Glenn Greibesland <glenn@glenng.no>
2017-04-15 15:50:57 +0200

    make title an @Input in app-toolbar, show only if defined

--- src/app/app-toolbar/app-toolbar.component.ts
+++ src/app/app-toolbar/app-toolbar.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class AppToolbarComponent implements OnInit {
   @Input() visible = true;
+  @Input() title: string;
 
   constructor() { }
 
