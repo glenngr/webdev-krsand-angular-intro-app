60efb9249f6dbdb9fdb6cf14ea5b5637edf9fb20
Glenn Greibesland <glenn@glenng.no>
2017-04-15 16:29:28 +0200

    create a Movie class with seen property and update ngFor loop

--- src/app/demos/demo1/demo1.component.ts
+++ src/app/demos/demo1/demo1.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Movie } from './movie.model';
+
 @Component({
   selector: 'app-demo1',
   templateUrl: './demo1.component.html',
   styleUrls: ['./demo1.component.css']
 })
 export class Demo1Component implements OnInit {
-  movieWatchlist: string[] = [
-    'Terminator 2',
-    'District 9',
-    'Get out'
+  movieWatchlist: Movie[] = [
+    new Movie('Terminator 2', true),
+    new Movie('District 9', true),
+    new Movie('Get out', false),
   ];
 
   constructor() { }
---
+++ src/app/demos/demo1/movie.model.ts
@@ -0,0 +1,11 @@
+// export class Movie {
+//     public title: string;
+//     public seen: boolean;
+// }
+
+export class Movie {
+    constructor(
+        public title: string,
+        public seen: boolean,
+    ) { }
+}
