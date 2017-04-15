9daea4b42b639f599c186434243982073193c7fa
Glenn Greibesland <glenn@glenng.no>
2017-04-15 16:49:24 +0200

    move "add movie" form to new component with @Output

---
+++ src/app/demos/demo1/add-movie/add-movie.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-add-movie',
+  templateUrl: './add-movie.component.html',
+  styleUrls: ['./add-movie.component.css']
+})
+export class AddMovieComponent implements OnInit {
+  @Output() movieAdded = new EventEmitter<string>();
+  public movieToAdd: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addMovie() {
+    this.movieAdded.emit(this.movieToAdd);
+  }
+}
--- src/app/demos/demos.module.ts
+++ src/app/demos/demos.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { Demo1Component } from './demo1/demo1.component';
+import { AddMovieComponent } from './demo1/add-movie/add-movie.component';
 
 @NgModule({
     imports: [CommonModule, FormsModule],
     exports: [Demo1Component],
-    declarations: [Demo1Component],
+    declarations: [Demo1Component, AddMovieComponent],
     providers: [],
 })
 export class DemoModule { }
