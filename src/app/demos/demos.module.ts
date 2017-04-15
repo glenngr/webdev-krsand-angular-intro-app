8fa9f66924d30ca614fe41eae736a5db87a0f69b
Glenn Greibesland <glenn@glenng.no>
2017-04-15 14:31:38 +0200

    show app title inside toolbar

--- src/app/demos/demos.module.ts
+++ src/app/demos/demos.module.ts
@@ -5,9 +5,7 @@ import { Demo1Component } from './demo1/demo1.component';
 @NgModule({
     imports: [],
     exports: [Demo1Component],
-    declarations: [Demo1Component,
-        Demo1Component
-    ],
+    declarations: [Demo1Component],
     providers: [],
 })
 export class DemoModule { }
