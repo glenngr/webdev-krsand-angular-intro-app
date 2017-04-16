3f6179f1f22207de072b71fccb67ee96282d6106
Glenn Greibesland <glenn@glenng.no>
2017-04-16 12:58:21 +0200

    created a new component for listing movies in watchlist

---
+++ src/app/demos/demo1/list-movies/list-movies.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from '../movie.model';
+
+@Component({
+  selector: 'app-list-movies',
+  templateUrl: './list-movies.component.html',
+  styleUrls: ['./list-movies.component.css']
+})
+export class ListMoviesComponent implements OnInit {
+  @Input() movieWatchlist: Movie[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/demos/demos.module.ts
+++ src/app/demos/demos.module.ts
@@ -6,11 +6,12 @@ import { Demo1Component } from './demo1/demo1.component';
 import { AddMovieComponent } from './demo1/add-movie/add-movie.component';
 import { MovieService } from './demo1/movie.service';
 import { HighlightDirective } from './demo1/highlight.directive';
+import { ListMoviesComponent } from './demo1/list-movies/list-movies.component';
 
 @NgModule({
     imports: [CommonModule, FormsModule],
     exports: [Demo1Component],
-    declarations: [Demo1Component, AddMovieComponent, HighlightDirective],
+    declarations: [Demo1Component, AddMovieComponent, HighlightDirective, ListMoviesComponent],
     providers: [MovieService],
 })
 export class DemoModule { }
